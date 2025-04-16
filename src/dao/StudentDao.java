73f2b18d475d8fc4d35ed6aaa6fc3c8a33b7f741
watanabeshiori <fko2447078@stu.o-hara.ac.jp>
2025-04-16 15:02:58 +0900

    あ

--- src/dao/StudentDao.java
+++ src/dao/StudentDao.java
@@ -1,3 +1,36 @@
 package dao;
 
-public StudentDao
+import java.sql.ResultSet;
+import java.util.List;
+
+import bean.School;
+import bean.Student;
+
+public class StudentDao extends Dao {
+	private String baseSql = "";
+	
+	public Student get(String no) throws Exception {
+		// 学生番号で学生情報取得
+		
+	}
+	private List<Student> postFilter(ResultSet rSet, School school) throws Exception {
+	    // ResultSetとSchoolオブジェクトを使って
+		// フィルタリング後の学生リストを生成する
+	}
+	public List<Student> filter(School school, int entYear, String classNum, boolean isAttend) throws Exception {
+	    // 学校、入学年、クラス番号、出席状況でフィルタリング
+	}
+
+	public List<Student> filter(School school, int entYear, boolean isAttend) throws Exception {
+		// 学校、入学年度、出席情報でフィルタリング
+	}
+
+	public List<Student> filter(School school, boolean isAttend) throws Exception {
+	    // 学校、出席状況でフィルタリング
+	}
+	public boolean save(Student student) throws Exception {
+	    // 学生情報をデータベースに保存するメソッド
+	}
+
+
+}
