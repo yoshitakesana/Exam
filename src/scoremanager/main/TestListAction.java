4039ecc4e1fda68377d2365aeb633972e3b962ed
y-uefune <fko2447020@stu.o-hara.ac.jp>
2025-05-28 10:43:42 +0900

    test

--- src/scoremanager/main/TestListAction.java
+++ src/scoremanager/main/TestListAction.java
@@ -1,5 +1,43 @@
 package scoremanager.main;
 
-public class TestListAction {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.TestListStudent;
+import dao.TestDao;
+
+@WebServlet("/TestListAction")
+public class TestListAction extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+            int entYear = Integer.parseInt(request.getParameter("entYear"));
+            String classNum = request.getParameter("classNum");
+            String subjectCd = request.getParameter("subjectCd");
+            int times = Integer.parseInt(request.getParameter("times"));
+
+            // 学生一覧を取得（DAOを使う）
+            TestDao testDao = new TestDao();
+            List<TestListStudent> studentList = testDao.searchStudentsForScoreInput(entYear, classNum);
+
+            // 検索条件も渡す
+            request.setAttribute("entYear", entYear);
+            request.setAttribute("classNum", classNum);
+            request.setAttribute("subjectCd", subjectCd);
+            request.setAttribute("times", times);
+            request.setAttribute("studentList", studentList);
+
+            RequestDispatcher rd = request.getRequestDispatcher("/test/test_regist_input.jsp");
+            rd.forward(request, response);
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
 }
